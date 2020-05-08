20e6afc8e1d9f0e304ff51f9aa010d098f068ff4
YAN Wenkun <code@yanwk.fun>
2020-05-08 09:23:12 +0800

    以依赖注入的形式添加 Greeting Service

--- src/main/java/fun/yanwk/hello/helloquarkus/GreetingResource.java
+++ src/main/java/fun/yanwk/hello/helloquarkus/GreetingResource.java
@@ -1,5 +1,8 @@
 package fun.yanwk.hello.helloquarkus;
 
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -12,6 +15,16 @@ import javax.ws.rs.core.MediaType;
  */
 @Path("/hello")
 public class GreetingResource {
+
+    @Inject
+    GreetingService service;
+
+    @GET
+    @Path("/greeting/{name}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String greeting(@PathParam String name) {
+        return service.greeting(name);
+    }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
---
+++ src/main/java/fun/yanwk/hello/helloquarkus/GreetingService.java
@@ -0,0 +1,12 @@
+package fun.yanwk.hello.helloquarkus;
+
+import javax.enterprise.context.ApplicationScoped;
+
+// 如果注释掉该注解，能正常编译，但启动即报错，也就过不了单元测试
+@ApplicationScoped
+public class GreetingService {
+
+    public String greeting(String name) {
+        return "Hello, " + name + "!";
+    }
+}
