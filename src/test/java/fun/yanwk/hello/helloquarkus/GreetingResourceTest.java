ff1febcbd31288b39f386006b49d595772e8cacf
YAN Wenkun <code@yanwk.fun>
2020-05-08 11:14:17 +0800

    修改测试用例

--- src/test/java/fun/yanwk/hello/helloquarkus/GreetingResourceTest.java
+++ src/test/java/fun/yanwk/hello/helloquarkus/GreetingResourceTest.java
@@ -3,19 +3,38 @@ package fun.yanwk.hello.helloquarkus;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.UUID;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
+/**
+ * 使用 QuarkusTest runner，会指示 JUnit 先启动程序，再执行测试
+ */
 @QuarkusTest
 public class GreetingResourceTest {
 
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("hello"));
+            .when()
+                .get("/hello")
+            .then()
+                .statusCode(200) // HTTP OK
+                .body(is("你好，世界！"));
+    }
+
+    // REST Assured 挺好使
+    @Test
+    public void testGreetingEndpoint() {
+        String uuid = UUID.randomUUID().toString();
+        given()
+                .pathParam("name", uuid)
+            .when()
+                .get("/hello/greeting/{name}")
+            .then()
+                .statusCode(200)
+                .body(is("Hello, " + uuid + "!"));
     }
 
 }
