ce6e1d5305a3ade4c536a598b5a2217e15d7f018
YAN Wenkun <code@yanwk.fun>
2020-05-08 09:08:36 +0800

    修改 Hello World 资源

--- src/main/java/fun/yanwk/hello/helloquarkus/GreetingResource.java
+++ src/main/java/fun/yanwk/hello/helloquarkus/GreetingResource.java
@@ -5,12 +5,17 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+/**
+ * 1. 在 Quarkus 中，独创建一个 Application 类并不是必须的。
+ * 2. 在 Quarkus 中，一个资源（REST术语）只创建一个实例，而不是每次请求创建一个。
+ * 3. 你可以用 *Scoped 注解来配置实例的创建关注点。
+ */
 @Path("/hello")
 public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "hello";
+        return "你好，世界！"; // 默认即无乱码
     }
 }
